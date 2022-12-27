aceb3cd0da9ea637b52c67094c5256779a3c2f31
Nelsonai <70864591+Nelsonai@users.noreply.github.com>
2022-12-27 14:51:48 -0800

    Add files via upload
    This just shows how to make an input line for a user

---
+++ Input/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Name
+{
+    class program{
+        static void Main(string[] args)
+        {
+            Console.Write("Type your name: ");
+
+            var input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input)){
+                Console.WriteLine("You didnt input a name.");
+                Environment.Exit(0);
+            }
+            Console.WriteLine("Hello, " + input);
+
+        }
+    }
+} 
