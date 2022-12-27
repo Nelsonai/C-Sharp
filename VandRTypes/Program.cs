f3a72e13b822a3177d0d433016bcb0ccbc10735a
Nelson_AI <kcneil1200@gmail.com>
2022-12-27 15:30:39 -0800

    Add files via upload

---
+++ VandRTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Name
+{
+    class program{
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = a;
+            b++;
+            // Output of a would be 10, and b would be 11\
+            Console.WriteLine(string.Format("a: {0}, b: {1}", a,b));
+
+            // But thats not the same with arrays
+
+            var array1 = new int[3] {1, 2, 3};
+            var array2 = array1;
+            array2[0] = 4;
+            Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
+        }
+    }    
+}
