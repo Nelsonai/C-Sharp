46bc0b2f31302ad5d0d85a15ea5bbff566c6bdf9
Nelson_AI <kcneil1200@gmail.com>
2022-12-27 15:14:44 -0800

    Add files via upload

---
+++ AdditionCLass/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Name
+{
+
+
+    public class addition{
+        public int add(int a, int b){
+            return a + b;
+        }
+    }
+    class program{
+        static void Main(string[] args){
+            var add1 = new addition();
+            var add2 = new addition();
+           var result = add1.add(1, 4);
+           Console.WriteLine(add2.add(2, 4));
+            Console.WriteLine(result); 
+        }
+    }
+}
---
+++ Array/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MyNamespace
+{
+    class program{
+        public static void Main(string[] args)
+        {
+            // int[] numbers = new int[3]; or
+            var numbers = new int[3];
+            numbers[0] = 1;
+            Console.WriteLine(numbers[0]); 
+
+            var names = new string[3] {"Kachi","Victor","Mary"};
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+            // STRING FORMATTING
+            // STRING LITERALS
+            var firstName = "Nelson-AI";
+            var lastName = "Kc-Neil";
+            // 1.-
+            var fullName = firstName + " " + lastName;
+            // 2.
+            string name = string.Format("My name is {0}  {1}", firstName, lastName);
+            Console.WriteLine(name);
+            // COMBINING ITEMS IN A LIST
+            var randomNames = new string[3] {"Ezekiel","micheal","odegard"};
+            var formattedNames = string.Join(",", randomNames);
+            Console.WriteLine(formattedNames);
+        }
+    }
+}
