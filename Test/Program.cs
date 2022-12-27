cfc9a6a34ab746acba6a465fe4b10c8c450607bd
Nelson_AI <kcneil1200@gmail.com>
2022-12-27 15:28:32 -0800

    Add files via upload

---
+++ Test/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Name
+{
+
+
+    public class name{
+        public string firstNames;
+        public string lastNames;
+        public int age;
+
+        public void introduce(){
+            Console.WriteLine("Her name is " + firstNames + " " + lastNames);
+            Console.WriteLine("age is "+ age);
+        }
+    }
+    class program{
+        static void Main(string[] args)
+        {
+            var Esther = new name();
+            Esther.firstNames = "Esther";
+            Esther.lastNames = "SPAGETTI";
+            Esther.age = 18;
+            Esther.introduce();
+        }
+    }
+}
